d9fb99fb46ee5795cb7ef99adb48646fe95acd3c
destino92 <destinodello@gmail.com>
2016-03-31 04:30:36 +0300

    added concat and sourcemaps module to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('scripts', function(){
 		return	gulp.src('js/*.js')
