a98e6d1c36c72287341a9565791424d466d38bd5
destino92 <destinodello@gmail.com>
2016-03-31 05:21:33 +0300

    modified scripts task and added it to default task

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,23 @@ var gulp = require('gulp'),
 	concatify = require('gulp-concat'),
 	sourcemaps = require('gulp-sourcemaps');
 
-/*gulp.task('scripts', function(){
+gulp.task('scripts', function(){
 	return gulp.src('js/*.js')
 		.pipe(sourcemaps.init())
 			.pipe(uglify())
-			.pipe(concatify('app.js'))
+			.pipe(concatify('permatters.js'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('dist/js'))
 });
-*/
+
 
 gulp.task('minify-css', function(){
-	return gulp.src('css/style.css').
+	return gulp.src('css/style.css')
 		.pipe(sourcemaps.init())
-			.pipe(cleanCSS())
+			.pipe(cleanCSS({compatibility: 'ie8'}))
 			.pipe(concatify('style.min.css'))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('dist/css/'));
 });
 
-gulp.task('default', ['minify-css']);
+gulp.task('default', ['minify-css','scripts']);
