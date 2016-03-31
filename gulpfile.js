8e97dc35644320f656f88e6064759dcc2ba31fcd
destino92 <destinodello@gmail.com>
2016-03-31 04:48:38 +0300

    added gulp-sourcemaps and gulp-clean-css module and minify-css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp'),
+	cleanCSS = require('gulp-clean-css');
 	uglify = require('gulp-uglify'),
-	concat = require('gulp-concat'),
+	concatify = require('gulp-concat'),
 	sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('scripts', function(){
-		return	gulp.src('js/*.js')
-						.pipe(uglify())
-						.pipe(gulp.dest('dist/js/'))
+/*gulp.task('scripts', function(){
+	return gulp.src('js/*.js')
+		.pipe(sourcemaps.init())
+			.pipe(uglify())
+			.pipe(concatify('app.js'))
+});
+*/
+
+gulp.task('minify-css', function(){
+	return gulp.src('css/style.css').
+		.pipe(sourcemaps.init())
+			.pipe(cleanCSS())
+			.pipe(concatify('style.min.css'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('dist/css/'));
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('default', ['minify-css']);
